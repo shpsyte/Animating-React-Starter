2fdc8786fda47f4ca5ea4268414e97a255df9c9d
luiz <jose.iscosistemas@gmail.com>
2023-02-21 10:45:48 -0800

    transition with mount and unmount components

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,41 +1,33 @@
 
 
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, animated } from 'react-spring/web'
 
 
 
 export default function Toggle() {
   const [isToggled, setToggle] = useState(false)
-  const {color, y} = useSpring({
- 
+  const transition = useTransition(isToggled, null, {
+     from: { opacity: 0},
+     enter: { opacity: 1 },
+     leave: { opacity: 0 },
     //  opacity: isToggled ? 1 : 0,
-
-    // opacity: isToggled ? 1 : 0,
-    // fontSize: isToggled ? '2rem' : '5rem',
-    color: isToggled ? 'red' : 'green',
-    // transform: isToggled ? 'translate3d(0,0,0)' : 'translate3d(0,-50px,0)',
-    y: isToggled ? 0 : 1,
-    
   })
-
-  console.log(' Am I rendering')
+ 
 
   const handleClick = () => {
     setToggle(a => !a)
   }
   return (
     <div>
-      <AnimatedTitle style={
-        {
-          transform: y.interpolate({
-            range: [0, 0.25, .50, .75, 1],
-            output: [0, -25, -50, -100, -50]
-          }).interpolate(y => `translate3d(0, ${y}px, 0)`),
-        // ...fade,
-        // transform: `translate3d(0,${fade.y}px,0)`,
-          color
-        }}>Hello</AnimatedTitle>
+      {transition.map(({ item, key, props: styles }) =>
+        item && (
+           <animated.h1 key={key} style={styles}>
+              Hello
+           </animated.h1>
+        )
+      )}
+      {/* <AnimatedTitle>Hello</AnimatedTitle> */}
       <button onClick={handleClick}>Toggle</button>
     </div>
   )
